4b8e72529e9f5964c56ae287bc96dd8a616b09e5
Neetish Jethra <njethra@zendesk.com>
2021-09-03 17:20:39 +1000

    Add median to stats

--- test_stats.py
+++ test_stats.py
@@ -12,5 +12,8 @@ class TestStats(unittest.TestCase):
     def test_mean(self):
         self.assertEqual(stats.get_mean([1, 2, 3, 4, 5]), 3)
 
+    def test_median(self):
+        self.assertEqual(stats.get_median([1, 2, 3, 4, 5]), 3)
+
 if __name__ == '__main__':
     unittest.main()
