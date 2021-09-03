bd5a784b3471ee1c62f8139a25dd801179201a5c
Neetish Jethra <njethra@zendesk.com>
2021-09-03 17:30:59 +1000

    Update tests

--- test_stats.py
+++ test_stats.py
@@ -4,13 +4,13 @@ import stats
 class TestStats(unittest.TestCase):
 
     def test_count(self):
-        self.assertEqual(stats.get_count([1,2,3,4]), 4)
+        self.assertEqual(stats.get_count([1, 2, 3, 4, 5]), 5)
 
     def test_sum(self):
-        self.assertEqual(stats.get_sum([1, 2, 3, 4]), 10)
+        self.assertEqual(stats.get_sum([1, 2, 3, 4, 5]), 15)
 
     def test_mean(self):
-        self.assertEqual(stats.get_mean([1, 2, 3, 4]), 2.5)
+        self.assertEqual(stats.get_mean([1, 2, 3, 4, 5]), 3)
 
 if __name__ == '__main__':
     unittest.main()
